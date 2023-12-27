3e611234f3841b985d996bb256c025fc2deadc1a
SHANIB C K <ckshanib8@gmail.com>
2023-12-27 12:36:29 +0530

    Money input field value fetch set

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
 const HomeScreen = () => {
-    const [Money, setMoney] = useState("")
-    const [Percentage, setPercentage] = useState("")
-    const [Year, setYear] = useState("")
+  const [Money, setMoney] = useState("");
+  const [Percentage, setPercentage] = useState("");
+  const [Year, setYear] = useState("");
 
-    
+  
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
@@ -19,6 +19,8 @@ const HomeScreen = () => {
             <div className="w-72">
               <div className="relative w-full min-w-[200px] h-10">
                 <input
+                  value={Money || ""}
+                  onChange={(e) => setMoney(e.target.value)}
                   type="number"
                   className="peer w-full h-full bg-transparent text-blue-gray-700 font-bold text-1xl outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-12 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-xl px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 border-white"
                   placeholder=" "
