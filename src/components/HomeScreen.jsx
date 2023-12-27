7d3aae202543fc69691e80bf37b8d3c9e22278a5
SHANIB C K <ckshanib8@gmail.com>
2023-12-27 11:14:45 +0530

    input field css added at index.css file

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -4,7 +4,11 @@ const HomeScreen = () => {
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
-        <div></div>
+        <div className="bg-white h-96 w-96 bg-opacity-30 rounded-2xl flex flex-col items-center justify-center">
+            <div className="in">
+                <input type="text" placeholder="Enter your place" />
+            </div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "40-r": "40rem",
+        "35-r": "35rem",
+        "30-r": "30rem",
+      },
+      height: {
+        "40-r": "40rem",
+        "35-r": "35rem",
+        "30-r": "30rem",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      boxShadow: {
+        "5xl": "20px 20px 50px rgba(0, 0, 0, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
