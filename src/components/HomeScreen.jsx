08d339b524ff3620f6b3921104ee3d94dfd88258
SHANIB C K <ckshanib8@gmail.com>
2023-12-27 12:27:32 +0530

    Success button designed

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -13,6 +13,7 @@ const HomeScreen = () => {
           <div className="w-72">
             <div className="relative w-full min-w-[200px] h-10">
               <input
+                type="number"
                 className="peer w-full h-full bg-transparent text-blue-gray-700 font-bold text-1xl outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-12 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-xl px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 border-white"
                 placeholder=" "
               />
@@ -25,6 +26,7 @@ const HomeScreen = () => {
           <div className="w-72">
             <div className="relative w-full min-w-[200px] h-10">
               <input
+                type="number"
                 className="peer w-full h-full bg-transparent text-blue-gray-700 font-bold text-1xl outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-12 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-xl px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 border-white"
                 placeholder=" "
               />
@@ -37,6 +39,7 @@ const HomeScreen = () => {
           <div className="w-72">
             <div className="relative w-full min-w-[200px] h-10">
               <input
+                type="number"
                 className="peer w-full h-full bg-transparent text-blue-gray-700 font-bold text-1xl outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-12 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-xl px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 border-white"
                 placeholder=" "
               />
@@ -46,7 +49,7 @@ const HomeScreen = () => {
             </div>
           </div>
           <div className="pt-5 flex flex-row">
-            <button className="bg-green-800">Calculate</button>
+            <button className="calc-btn">Calculate</button>
             <button>Reset</button>
           </div>
         </div>
