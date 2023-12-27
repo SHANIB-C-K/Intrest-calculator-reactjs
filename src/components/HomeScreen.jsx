05701ff3df1df6c54c79db64cc84724d2233409c
SHANIB C K <ckshanib8@gmail.com>
2023-12-27 12:45:13 +0530

    Year input field value fetch set

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -57,6 +57,8 @@ const HomeScreen = () => {
             <div className="w-72">
               <div className="relative w-full min-w-[200px] h-10">
                 <input
+                  value={Year}
+                  onChange={(e) => setYear(e.target.value)}
                   type="number"
                   className="peer w-full h-full bg-transparent text-blue-gray-700 font-bold text-1xl outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-12 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-xl px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 border-white"
                   placeholder=" "
