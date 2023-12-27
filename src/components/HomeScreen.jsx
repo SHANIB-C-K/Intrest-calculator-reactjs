9ae39792a5d47e69af467f12e45400fb8ddcbc12
SHANIB C K <ckshanib8@gmail.com>
2023-12-27 18:12:46 +0530

    Shadow container bg color changed

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -19,11 +19,14 @@ const HomeScreen = () => {
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
-        <div className="bg-white h-auto w-96 bg-opacity-30 rounded-2xl flex flex-col items-center justify-center py-3 pb-5">
-          <div className="font-bold text-2xl text-white">
+        <div className="bg-black h-auto w-96 bg-opacity-80 rounded-2xl flex flex-col items-center justify-center py-3 pb-5 shadow-5xl shadow-blue-950">
+          <div className="font-bold text-2xl text-white pt-3">
             Intrest Calculator
           </div>
-          <div className="font-bold text-black text-4xl">₹ {Intrest}</div>
+          <div className="font-bold text-green-700 text-4xl">₹ {Intrest}</div>
+          <p className="text-lg text-red-500 font-bold pt-3">
+            Calculate Your Simple Interest Easily
+          </p>
           <form onSubmit={intrestCalculator}>
             <div className="pb-4"></div>
             <div>
@@ -36,9 +39,9 @@ const HomeScreen = () => {
               <input
                 value={Money || ""}
                 onChange={(e) => setMoney(e.target.value)}
-                type="₹ Amount"
+                type="number"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your Money"
+                placeholder="₹ Amount"
                 required
               />
             </div>
